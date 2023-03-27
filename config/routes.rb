481751bb5ef000b53068d5b38937a37248fb03d0
Sam Wells <sfwells0518@gmail.com>
2023-03-27 15:26:16 -0500

    add route for guess_number

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "/guess_number", controller: "my_practice", action: "guess_number"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
